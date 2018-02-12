f85bee92be6572955f62c7c9ec0fda39095d5b03
Kevin Wells <nivek706@gmail.com>
2018-02-11 21:16:55 -0600

    init

---
+++ dashboard/Player.js
@@ -0,0 +1,7 @@
+function Player(name, social, state, league, description) {
+	this.name = name;
+	this.social = social;
+	this.state = state;
+	this.league = league;
+	this.description = description;
+}
---
+++ dashboard/PlayerListMWC14.js
@@ -0,0 +1,7 @@
+function PlayerListMWC14() {
+	this.contents = [];
+	this.contents.push(new Player("GAMETIME", "@GAMETIMESC2", "Michigan", "Grandmaster", "<ul><li>Has placed top 4 in every MWC he has entered (8 times!)</li><li>Recently achieved WCS Challenger status</li></ul>"));
+	this.contents.push(new Player("Rengen", "@BradyCassada", "Wisconsin", "Grandmaster", "<ul><li>Started as a Diamond player in MWC5</li><li>Freshly recruited to DankShrinePro squad</li></ul>"));
+	this.contents.push(new Player("VampyWorm", "@VampyWorm", "Wisconsin", "Master", "<ul><li>Top 15 teams in CSL CS D2</li><li>Still not GM (FeelsBadMan)</li></ul>"));
+	this.contents.push(new Player("Gravity", "@gravity620", "Missouri", "Grandmaster", "<ul><li>Recently placed 2nd in 50$ Starbow Open, lost to Arthur 1-2</li><li>Good friends with Pandabearme, terrified of Gametime</li><li>Has been playing SC2 since he was 11, WoL Alpha</li></ul>"));
+}
