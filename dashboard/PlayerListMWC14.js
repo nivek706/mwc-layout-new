7e2cb4de99c77b7062c48e77fe40b8f39bf4371c
Kevin Wells <nivek706@gmail.com>
2018-02-17 23:25:13 -0600

    Updated for Midwest Cup 14

--- dashboard/PlayerListMWC14.js
+++ dashboard/PlayerListMWC14.js
@@ -3,5 +3,14 @@ function PlayerListMWC14() {
 	this.contents.push(new Player("GAMETIME", "@GAMETIMESC2", "Michigan", "Grandmaster", "<ul><li>Has placed top 4 in every MWC he has entered (8 times!)</li><li>Recently achieved WCS Challenger status</li></ul>"));
 	this.contents.push(new Player("Rengen", "@BradyCassada", "Wisconsin", "Grandmaster", "<ul><li>Started as a Diamond player in MWC5</li><li>Freshly recruited to DankShrinePro squad</li></ul>"));
 	this.contents.push(new Player("VampyWorm", "@VampyWorm", "Wisconsin", "Master", "<ul><li>Top 15 teams in CSL CS D2</li><li>Still not GM (FeelsBadMan)</li></ul>"));
-	this.contents.push(new Player("Gravity", "@gravity620", "Missouri", "Grandmaster", "<ul><li>Recently placed 2nd in 50$ Starbow Open, lost to Arthur 1-2</li><li>Good friends with Pandabearme, terrified of Gametime</li><li>Has been playing SC2 since he was 11, WoL Alpha</li></ul>"));
+	this.contents.push(new Player("Gravity", "@realgravity620", "Missouri", "Diamond", "<ul><li>Also a shoutcaster for Psionic Aftermath</li></ul>"));
+	this.contents.push(new Player("Noticals", "@NewGuyNoticals", "Kansas", "Diamond", "<ul><li>Managed to eat a whole rotisserie chicken in one sitting</li></ul>"));
+	this.contents.push(new Player("AlbinoShadow", "twitch.tv/albinoshadow", "Michigan", "Master", "<ul><li>Went to university at 15</li><li>MWC14 will be a return from a StarCraft hiatus</li></ul>"));
+	this.contents.push(new Player("EON", "@EONsc2", "Minnesota", "Grandmaster", "<ul><li>Top 4 Passioncraft Season 6</li></ul>"));
+	this.contents.push(new Player("Farmcakes", "@Farmcakes150", "Wisconsin", "Grandmaster", "<ul><li>Champion of MidWest LAN 18</li></ul>"));
+	this.contents.push(new Player("Ukko", "@UkkoSC2", "Michigan", "Grandmaster", "<ul><li>This will be his first MWC appearance</li></ul>"));
+	this.contents.push(new Player("PandaBearMe", "@PandaBearMeSC2", "Wisconsin", "Grandmaster", "<ul><li>Recently played in IEM Pyeongchang</li><li>Champion of the last 4 Midwest Cups</li></ul>"));
+	this.contents.push(new Player("Toast", "@DankShrine", "Minnesota", "Platinum", "<ul><li>Godlike alternative strategies (cheese)</li><li>Runs the DankShrine talkshow and SC2 squad</li></ul>"));
+	this.contents.push(new Player("AprilTerran", "@travisyang", "Missouri", "Diamond", ""));
+	this.contents.push(new Player("Tambi", "", "", "Grandmaster", "<ul><li>9th place Dreamhack Atlanta 2017</li><li>First Midwest Cup appearance</li></ul>"));
 }
